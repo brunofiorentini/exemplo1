e5a54748c8b3cb074fe45049a9280ded307e7412
brunofiorentini <brunofiorentini93@gmail.com>
2015-02-21 11:26:37 -0200

    comitando o projeto inicial

---
+++ src/main/java/demo/controllers/Aluno.java
@@ -0,0 +1,36 @@
+package demo.controllers;
+
+public class Aluno {
+	
+	private String nome;
+	private Integer ra;
+	
+	public Aluno(String nome, Integer ra) {
+		this.nome = nome;
+		this.ra = ra;
+	}
+	public Aluno() {
+		
+	}
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getRa() {
+		return ra;
+	}
+
+	public void setRa(Integer ra) {
+		this.ra = ra;
+	}
+	
+	
+	
+
+}
